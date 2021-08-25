15a11b812a8824bde17bfd5c287687e2b917ef74
Maulwurfn <39377864+Maulwurfn@users.noreply.github.com>
2021-08-25 21:45:07 +0200

    algo working

--- mergesort/ListExtensions.cs
+++ mergesort/ListExtensions.cs
@@ -8,9 +8,62 @@ namespace mergesort
 {
     public static class ListExtensions
     {
+
         public static List<int> MergeSort(this List<int> list)
         {
-            return list;
+            if (list.Count <= 1)
+            {
+                // the fuck shall i sort?
+                return list;
+            }
+
+            var median = list.Count / 2;
+
+            var left = new List<int>();
+            for (int index = 0; index < median; index++)
+            {
+                left.Add(list[index]);
+            }
+
+            var right = new List<int>();
+            for (int index = median; index < list.Count; index++)
+            {
+                right.Add(list[index]);
+            }
+
+            left = MergeSort(left);
+            right = MergeSort(right);
+
+            var result = new List<int>();
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First() <= right.First())
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());                        
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    // right side is empty dump all
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    // left side is empty dump all
+                    result.Add(right.First());
+                    right.Remove(right.First());
+                }
+            }
+            return result;
         }
     }
 }
