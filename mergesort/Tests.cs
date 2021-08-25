91c90d4eb5aadf3fe1f02bbc07fe672d47ee0cb8
Maulwurfn <39377864+Maulwurfn@users.noreply.github.com>
2021-08-25 20:44:17 +0200

    test added

---
+++ mergesort/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace mergesort
+{
+    public class Tests
+    {
+        [Fact]
+        public void ListSorted()
+        {
+            var unsorted = new List<int> { 3, 12, 5, 1, 7, };
+            var expected = new List<int> { 1, 3, 5, 7, 12 };
+            var actual = unsorted.MergeSort();
+            Assert.True(actual.SequenceEqual(expected));
+        }
+    }
+}
